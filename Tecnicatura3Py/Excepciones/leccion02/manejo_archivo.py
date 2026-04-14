e36e76d6d2210d23013591c195779a9cec71279e
ruben-marchisio <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>
2026-04-14 00:06:56 -0300

    cargo la clase 2 de py los videos 1,2 y 3

---
+++ Tecnicatura3Py/Excepciones/leccion02/manejo_archivo.py
@@ -0,0 +1,23 @@
+#1.1 Introducción a lo que es el manejo de archivos
+#1.2 Especificar el juego de caracteres de un archivo de texto
+#Declaramos una variable
+
+# video 1,2 y 3
+
+try:
+    archivo = open("archivo.txt", "w", encoding="utf-8") #w = write (escritura)
+    archivo.write("programamos con diferentes tipos de archivos\n")
+    archivo.write("los acentos son importates para las palabras\n")
+    archivo.write("como por ejemplo: acción, corazón, canción\n")
+    archivo.write("las letras son: r = read (lectura), \nw = write (escritura), \na = append (agregar) \nx = close (cerrar)\n")
+    archivo.write("t = texto, \nb = binario, \nw+ = escritura y lectura, \nr+ = lectura y escritura\n")
+    archivo.write("saludos a todos los programadores\n")
+    archivo.write("con esto terminamos")
+except Exception as e:
+    print(e)
+finally: # siempre se ejecuta sin importar si hubo error o no
+    archivo.close() #cerramos el archivo
+    
+
+# archivo.write("hola") # si se ejecuta esto se borra todo lo anterior
+
