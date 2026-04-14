80ed1113b7da1d90e03082e2b8df4c54dceec5cd
micaela <aleacimoyamara@gmail.com>
2026-04-14 04:32:11 -0300

    Agrego carpeta leccion02 de JavaScript y primer video Modo Estricto

---
+++ Tecnicatura3JS/Leccion02/02-ModoStrict.js
@@ -0,0 +1,13 @@
+//"use strict";
+const x = 10; 
+console.log(x);
+
+
+function miFuncion(){
+    "use strict";
+    let  y = 10;
+     console.log(y);
+}
+miFuncion();
+//Modo estricto no permite tener ninguna variable sin declarar
+//Dentro de una funcion puede ser diferente. Se puede usar al comienzo del codigo
