84f39b96fbc02412710fe21ead40774dee86909a
Jimena K Perez <jime3011@gmail.com>
2026-04-29 21:07:19 -0300

    Se trabaja en clase 3 Base de datos

--- Tecnicatura3Py/Leccion2/Leer_archivo.py
+++
@@ -1,7 +0,0 @@
-
-archivo = open('prueba.txt', 'r', encoding='utf8')
-#print(archivo.read())
-#print(archivo.read(16))
-#print(archivo.read(10)) # continuamos desde la linea anterior
-print(archivo.readline())
-print(archivo.readline())
