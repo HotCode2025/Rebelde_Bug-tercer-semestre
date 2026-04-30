67ad8da709a73dcd3c49add24ec872e51a8419a7
micaela <aleacimoyamara@gmail.com>
2026-04-30 02:36:11 -0300

    Agrego clases de Sobreescritura y Polimorfismo

---
+++ Tecnicatura3Java/Leccion04/Gerente.java
@@ -0,0 +1,21 @@
+
+package domain;
+
+public class Gerente extends Empleado {
+    private String departamento;
+    
+    public Gerente(String nombre, double sueldo, String departamento){
+        super(nombre, sueldo);//llamado del constructor de la clase padre
+        this.departamento = departamento;
+        
+        
+    }
+    //Sobreescribimos el método
+    @Override 
+    public String obtenerDetalles(){
+        return super.obtenerDetalles()+", Departamento: "+this.departamento;
+        
+    }
+    
+    
+}
---
+++ Tecnicatura3Java/Leccion04/TestSobreescritura.java
@@ -0,0 +1,23 @@
+
+package test;
+import domain.*;
+
+
+public class TestSobreescritura {
+    public static void main(String[] args){
+        Empleado empleado1 = new Empleado("Juan", 10000);
+        imprimir(empleado1);
+        //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
+        
+        empleado1 = new Gerente("Jose", 5000, "Sistemas"); //objeto clase hija
+        imprimir(empleado1);//apunta a una referencia de la clase hija
+        //System.out.println("gerente1 = "+gerente1.obtenerDetalles());//No muestra lo que es el departamento porque en la clase padre no está incluido el departamento
+    }
+    
+    //Ejecutamos el método obtener detalles
+    public static void imprimir(Empleado empleado){//a través de la herencia la referencia que recibe no es de tipo empleado, es de tipo gerente
+        String detalles = empleado.obtenerDetalles();
+        System.out.println("detalles = " + detalles);//llama al método sobreescrito en la clase hija
+    }
+    
+}
