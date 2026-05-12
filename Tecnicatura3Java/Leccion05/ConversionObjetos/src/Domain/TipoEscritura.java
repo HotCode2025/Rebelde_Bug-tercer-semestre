54e770dd1fac68d2b5010c1db5b3a5a1c73d754e
Melina Gallo <melinadenisegallo@gmail.com>
2026-05-12 16:26:43 -0300

    Clase 5 Casting

--- Tecnicatura3Java/Leccion05/ConversionObjetos/src/Domain/TipoEscritura.java
+++ Tecnicatura3Java/Leccion05/ConversionObjetos/src/Domain/TipoEscritura.java
@@ -9,6 +9,18 @@ package Domain;
  *
  * @author mmenu
  */
-public class TipoEscritura {
+public enum TipoEscritura {
+    CLASICO("Escritura a mano");
+    MODERNO("Escritura digital");
     
+    private final String descripcion;
+    
+    private TipoEscritura(String descripcion){//Constructor
+        this.descripcion = descripcion;
+    }
+    
+    //Método get
+    public String getDescripcion(){
+        return this.descripcion;
+    }
 }
