193f3fbae47ab2a26c8463e9e9178173ef8aa7ec
Melina Gallo <melinadenisegallo@gmail.com>
2026-05-21 10:54:31 -0300

    Codigo de juego avatar listo para tarea de asignar seleeccion de cada personaje

---
+++ Tecnicatura3JS/Avatar/public/js/avatar.js
@@ -0,0 +1,6 @@
+function selecionarPersonajeJugador(){
+    alert ('Seleccionaste tu personaje')
+}
+
+let botonPersonajeJugador = document.getElementById('boton-personaje')
+botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador);
