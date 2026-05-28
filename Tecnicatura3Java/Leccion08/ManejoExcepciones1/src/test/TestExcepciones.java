c5aadeeed119d4d6df0281412f89b0d121de7ef2
Melina Gallo <melinadenisegallo@gmail.com>
2026-05-28 11:06:09 -0300

    8.3 Comenzamos Con Excepciones -> Parte 1, 2

--- Tecnicatura3Java/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Tecnicatura3Java/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -3,8 +3,14 @@ package test;
 
 public class TestExcepciones {
     public static void main(String[] args) {
-        int resultado =10/0;
-        System.out.PrintIn("resultado =" + resultado);
+        int resultado = 0;
+        try {
+            resultado = 10/0;
+        } catch(Exception e){
+            System.out.PrintIn("Ocurrió un Error");
+            e.printStackTrace(System.out); //Se conoce como la pila de excepciones
+        }
+        System.out.PrintIn("La variable de resultado tiene como valor: "+resultado);
     }
     
 }
